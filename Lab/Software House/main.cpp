6404b656243621034078e8bec66b8880b6187bba
ngntrgduc <trungducnguyen1511@gmail.com>
2023-07-25 16:33:40 +0700

    Update all

--- Lab/Software House/main.cpp
+++ Lab/Software House/main.cpp
@@ -1,59 +1,62 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
+#include <unordered_map>
 
 using namespace std;
 
 class Employee;
 class Programmer;
 class ProjectLeader;
+class SoftwareHouse;
 
 class Employee {
-    string payroll;
-    int monthlySalary;
+    string number;
+    int salary;
     string name;
 public:
-    Employee(string payroll, int monthlySalary, string name) {
+    Employee(string number, int salary, string name) {
+        this->setNumber(number);
+        this->setSalary(salary);
         this->setName(name);
-        this->setPayroll(payroll);
-        this->setSalary(monthlySalary);
     }
 
-    string getPayroll() {
-        return this->payroll;
+    string getNumber() {
+        return this->number;
     }
 
-    int getMonthlySalary() {
-        return this->monthlySalary;
+    int getSalary() {
+        return this->salary;
     }
 
-    string getName() {
+    virtual string getName() {
         return this->name;
     }
 
-    void setPayroll(string payroll) {
-        this->payroll = payroll;
+    void setNumber(string number) {
+        this->number = number;
     }
 
     void setSalary(int salary) {
-        this->monthlySalary = salary;
+        this->salary = salary;
     }
 
     void setName(string name) {
         this->name = name;
     }
 
-    void display() {
-        cout << "Number: " << this->getPayroll() << 
-                ", Salary: " << this->getMonthlySalary() <<
-                ", Name: " << this->getName() << '\n';
+    virtual void display() {
+        cout << "- Number: " << this->getNumber() << 
+                "\n  Salary: " << this->getSalary() <<
+                "\n  Name: " << this->getName() << '\n';
     }
 };
 
 class Programmer : public Employee {
     string language;
 public:
-    Programmer(string payroll, int monthlySalary, string name, string language) : Employee(payroll, monthlySalary, name) {
+    Programmer(string number, int salary, string name, string language) : Employee(number, salary, name) {
         this->setLanguage(language);
     }
 
@@ -64,45 +67,93 @@ public:
     void setLanguage(string language) {
         this->language = language;
     }
-    void display() {
-        cout << "Number: " << this->getPayroll() << 
-                ", Salary: " << this->getMonthlySalary() <<
-                ", Name: " << this->getName() << 
-                ", Language: " << this->getLanguage() << '\n';
+
+    virtual void display() {
+        Employee::display();
+        cout << "  Language: " << this->getLanguage() << '\n';
+    }
+
+    string getName() {
+        return Employee::getName();
     }
 };
 
-class ProjectLeader {
-
+class ProjectLeader: public Programmer {
+    unordered_map<string, Programmer*> team;
 public:
-    void addProgrammer(Programmer programmer) {
+    ProjectLeader(string number, int salary, string name, string language) 
+        : Programmer(number, salary, name, language) {}
 
+    void addProgrammer(Programmer *programmer) {
+        this->team.insert({programmer->getNumber(), programmer});
     }
-    void display() {
 
+    void display() {
+        cout << " --- Project Leader: \n";
+        Programmer::display();
+        cout << "+ Have programmers:\n";
+        // for (auto it = this->team.begin(); it != this->team.end(); it++)
+        //     it->second->display();
+        for (auto programmer: team) 
+            programmer.second->display();
+    }
+
+    string getName() {
+        return Employee::getName();
+    }
+};
+
+class SoftwareHouse {
+    string name;
+    map<string, Programmer*> staffs;
+public:
+    SoftwareHouse(string name) {
+        this->setName(name);
+    }
+    SoftwareHouse() : SoftwareHouse("NA"){}
+
+    string getName() {
+        return this->name;
+    }
+
+    void setName(string name) {
+        this->name = name;
+    }
+
+    void addProgrammer(Programmer *programmer) {
+        this->staffs.insert({programmer->getNumber(), programmer});
+        // this->staffs.emplace(programmer->getNumber(), programmer);
+    }
+
+    void displayStaff() {
+        cout << "Software House: " << this->getName() << "\n";
+        for (auto it = this->staffs.begin(); it != this->staffs.end(); it++) {
+            // cout << it->second->getName() << endl;
+            it->second->display();
+        }
     }
 };
 
 int main() {
     // Create some new programmers.
-    Programmer* p1 = new Programmer("123", 2000, "John", "Ada");
-    Programmer* p2 = new Programmer("234", 2500, "Ken", "C++");
-    Programmer* p3 = new Programmer("456", 3000, "Peter", "Java");
-    
+    Programmer p1("123", 2000, "John", "Ada");
+    Programmer p2("234", 2500, "Ken", "C++");
+    Programmer p3("456", 3000, "Peter", "Java");    
+
     // Create some new project leaders.
-    // ProjectLeader pl1("567", 4000, "Jon", "C");
-    // ProjectLeader pl2("789", 4000, "Jessie", "Java");
+    ProjectLeader pl1("567", 4000, "Jon", "C");
+    ProjectLeader pl2("789", 4000, "Jessie", "Java");
 
     // Assign each programmer to a project leader
-    // pl1.addProgrammer(p3);
-    // pl1.addProgrammer(p2);
-    // pl2.addProgrammer(p1);
-    // pl1.display();
-    // pl2.display();
+    pl1.addProgrammer(&p3);
+    pl1.addProgrammer(&p2);
+    pl2.addProgrammer(&p1);
 
-    p1->display();
-    p2->display();
-    p3->display();
-
-    return 0;
+    SoftwareHouse house("Mai am gia dinh");
+    house.addProgrammer(&p2);
+    house.addProgrammer(&p1);
+    house.addProgrammer(&p3);
+    house.addProgrammer(&pl1);
+    house.addProgrammer(&pl2);
+    house.displayStaff();
 }
--- test.cpp
+++ test.cpp
@@ -1,48 +1,162 @@
-#include <iostream>
-#include <string>
+#include<iostream>
+#include<string>
+#include<map>
+#include<vector>
 
 using namespace std;
 
-// template <typename T>
+class Person;
+class Vaccine;
 
-// // class Bruh {
-// // public:
-// //     Bruh() {}
-// //     T lmao(T lmao) {
-// //         return lmao;
-// //     }
-// // };
+class Person
+{
+    private:
+        string id;
+        string name;
+        string date;
+        vector<pair<Vaccine*, pair<string, string>>> immunizations;
 
-// T add(T a, T b) {
-//         return a + b;
-// }
+    public:
 
-// int main() {
-//     int a = 2, b = 4;
-//     string c = "hello", d = " world";
-//     cout << add(a, b);
-//     cout << add(c, d);
-// }
+        //Constructor
+        Person(string id, string name, string date)
+        {
+            this->setId(id);
+            this->setName(name);
+            this->setDate(date);
+        }
 
-class Bruh {
-    int lmao;
-public:
-    Bruh(int lmao) {
-        lmao = lmao;
-    }
+        //Defaut constructor
+        Person(string id) : Person(id, "NA", "NA") {};
+        Person() : Person ("???", "NA", "NA") {};
 
-    Bruh(const Bruh& bruh) {
-        lmao = bruh.lmao;
-    }
+        //Destructor
+        ~Person() {};
 
-    void get() {
-        cout << lmao << '\n';
+        //Access methods: Getters and Setters
+        void setId(string id) 
+        {
+            this->id = id;
+        }
+        const string& getId() const
+        {
+            return id;
+        }
+        void setName(string name)
+        {
+            this->name = name;
+        }
+        const string& getName() const
+        {
+            return name;
+        }
+        void setDate(string date)
+        {
+            this->date = date;
+        }
+        const string& getDate() const 
+        {
+            return date;
+        }
+
+        //Other methods
+        void addImmunization(Vaccine* vaccine, string dose, string date) 
+        {
+            immunizations.push_back(make_pair(vaccine, make_pair(dose, date)));
+        }
+        void printCertificate() 
+        {
+        cout << "ID: " << this->getId() << endl;
+        cout << "Date: " << this->getDate() << endl;
+        cout << "Information immunization of " << getName() << ":" << endl;
+        for (auto immunization : immunizations) {
+            cout << "Injection: " << immunization).first << ", Dose: " << immunization.second.first << ", Date: " << immunization.second.second << endl;
+        }
+        cout << "==========================" << endl;
     }
 };
 
+class Vaccine 
+{
+    private:
+        int idVac;
+        string dateVac;
+
+    public:
+
+        //Constructor
+        Vaccine(int idVac, string dateVac)
+        {
+            this->setId(idVac);
+            this->setDate(dateVac);
+        }
+
+        //Defaut constructor
+        Vaccine(int id) : Vaccine(id, "NA") {};
+        Vaccine() : Vaccine(0, "NA") {};
+
+        //Destructor
+        ~Vaccine() {};
+
+        //Access methods: Getters and Setters
+        void setId(int idVac)
+        {
+            this->idVac = idVac;
+        }
+        const int& getId() const
+        {
+            return idVac;
+        }
+        void setDate(string dateVac)
+        {
+            this->dateVac = dateVac;
+        }
+        const string& getDate() const
+        {
+            return dateVac;
+        }
+};
+
+class Pfizer : public Vaccine 
+{
+    public:
+        Pfizer(int id, string date) : Vaccine(id, date) 
+        {}
+        string nameVac = "Pfizer";
+};
+
+class Moderna : public Vaccine 
+{
+    public:
+        Moderna(int id, string date) : Vaccine(id, date) 
+        {}
+        string nameVac = "Moderna";
+};
+
 int main() {
-    Bruh a(1);
-    Bruh b(a);
-    a.get();
-    b.get();
+
+    //Create person
+    Person person1("04503213123", "Nguyen Huu Minh", "1985");
+    Person person2("05213123213", "Nguyen Van Huy", "1990");
+
+    //Create Vaccine
+    Pfizer pfizer1(1, "2021");
+    Pfizer pfizer2(2, "2021");
+    Moderna moderna1(3, "2022");
+    Moderna moderna2(4, "2022");
+
+    //Add Immunization to person
+    person1.addImmunization(&pfizer1, "Dose 1", "01/06/2022");
+    person1.addImmunization(&pfizer2, "Dose 2", "15/02/2023");
+
+    person2.addImmunization(&moderna1, "Dose 1", "02/06/2022");
+    person2.addImmunization(&moderna2, "Dose 2", "01/03/2023");
+
+    //Print Certificate of Immunization
+    person1.printCertificate();
+
+    person2.printCertificate();
+
+
+    return 0;
 }
